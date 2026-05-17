7451a9a6eaa339cea559cf38fb5fb668eb2608d9
PhattadonBS <phattadon232@gmail.com>
2026-05-17 21:26:16 +0700

    feat: add jwt for security

--- src/routes/router_api.ts
+++ src/routes/router_api.ts
@@ -5,6 +5,7 @@ import * as userController from '../controllers/user_api';
 import * as dormController from '../controllers/dorm_api';
 import * as testApi from '../controllers/testApi'
 import rateLimit from "express-rate-limit";
+import { verifyToken } from "../middlewares/auth_middleware";
 
 
 const upload = multer({ storage: multer.memoryStorage() });
@@ -46,13 +47,13 @@ router.post('/api/test2', testApi.test_send);
 router.post('/api/user/registerSec1', userController.registerSec1);//pass
 router.post('/api/user/registerSec2', userController.registerSec2);//pass
 router.put('/api/user/resetPassword', userController.resetPassword_api);//pass
-router.get('/api/user/users', userController.getUsers_api);//pass
-router.get('/api/user/members', userController.getMembers_api);//pass
-router.get('/api/user/dormOwners', userController.getDormOwners_api);//pass
-router.post('/api/user/dormOwner', upload.single("file"), userController.requestDormOwner_api);//pass
-router.put('/api/user/approve', userController.approveDormOwner);//pass
-router.post('/api/user/review', dormController.addReview_api);//pass
-router.get('/api/user/dormOwnerReq', dormController.getPendingOwners_api);//pass
+router.get('/api/user/users', verifyToken, userController.getUsers_api);//pass
+router.get('/api/user/members', verifyToken, userController.getMembers_api);//pass
+router.get('/api/user/dormOwners', verifyToken, userController.getDormOwners_api);//pass
+router.post('/api/user/dormOwner', verifyToken, upload.single("file"), userController.requestDormOwner_api);//pass
+router.put('/api/user/approve', verifyToken, userController.approveDormOwner);//pass
+router.post('/api/user/review', verifyToken, dormController.addReview_api);//pass
+router.get('/api/user/dormOwnerReq', verifyToken, dormController.getPendingOwners_api);//pass
    
 
 // auth group
@@ -64,36 +65,36 @@ router.post('/api/auth/recoverAccount', userController.recoverAccount_api)//pass
 
 // other data groupt 
 router.post('/api/other/mailSenter', userController.resMailSender_api);//pass
-router.post('/api/other/addFavorite', userController.addFavorite_api);//pass
-router.delete('/api/other/delFavorite', userController.removeFavorite_api);//pass
+router.post('/api/other/addFavorite', verifyToken, userController.addFavorite_api);//pass
+router.delete('/api/other/delFavorite', verifyToken, userController.removeFavorite_api);//pass
 
 // ✅ Dormitory group
-router.get('/api/dorms/pendingReq', dormController.getPendingDormReq_api);//pass
+router.get('/api/dorms/pendingReq', verifyToken, dormController.getPendingDormReq_api);//pass
 router.get('/api/dorms/zones', dormController.getAllZones); 
 router.get('/api/dorms', dormController.getAllDorms);    
-router.get('/api/dorms/admin', dormController.getAllDorms_Admin);//pass    
+router.get('/api/dorms/admin', verifyToken, dormController.getAllDorms_Admin);//pass    
 router.get('/api/dorms/popular', dormController.getPopularDorms_api);//pass        
-router.post('/api/dorms', imgTypeUploads, dormController.createDorm_api);//pass
-router.post('/api/dorms/approve', dormController.approveDormReq_api);//pass
-router.post('/api/dorms/facility', upload.single("fac"), dormController.addFacility_api);//pass
+router.post('/api/dorms', verifyToken, imgTypeUploads, dormController.createDorm_api);//pass
+router.post('/api/dorms/approve', verifyToken, dormController.approveDormReq_api);//pass
+router.post('/api/dorms/facility', verifyToken, upload.single("fac"), dormController.addFacility_api);//pass
 router.get('/api/dorms/facilities', dormController.getFacilities_api);//pass
 
 
 //specific data group
-router.get('/api/spec/user/:id', userController.getUser_api)//pass
-router.get('/api/spec/favorite/:id', userController.getMyFavorites_api)//pass
-router.delete('/api/spec/dorm/:id', dormController.removeDorm_api)//pass
-router.put('/api/spec/restoreDorm/:id', dormController.restoreDorm_api)//pass
-router.put('/api/spec/user/:id', userController.updateUser_api)//pass
-router.delete('/api/spec/delAccount/:id', userController.deleteAccount_api)//pass
-router.put('/api/spec/banAccount/:id', userController.banAccount_api)//pass
-router.put('/api/spec/dorm/:id', imgTypeUploads, dormController.updateDorm_api)//pass
+router.get('/api/spec/user/:id', verifyToken, userController.getUser_api)//pass
+router.get('/api/spec/favorite/:id', verifyToken, userController.getMyFavorites_api)//pass
+router.delete('/api/spec/dorm/:id', verifyToken, dormController.removeDorm_api)//pass
+router.put('/api/spec/restoreDorm/:id', verifyToken, dormController.restoreDorm_api)//pass
+router.put('/api/spec/user/:id', verifyToken, userController.updateUser_api)//pass
+router.delete('/api/spec/delAccount/:id', verifyToken, userController.deleteAccount_api)//pass
+router.put('/api/spec/banAccount/:id', verifyToken, userController.banAccount_api)//pass
+router.put('/api/spec/dorm/:id', verifyToken, imgTypeUploads, dormController.updateDorm_api)//pass
 router.get('/api/spec/dorm/:id', dormController.getDormsByOwner_api)//pass
-router.delete('/api/spec/review/:id', dormController.deleteReview_api)//pass
+router.delete('/api/spec/review/:id', verifyToken, dormController.deleteReview_api)//pass
 router.get('/api/dorms/review/:id', dormController.getReviewsByDormId_api);//pass
 router.get('/api/dorms/:id', dormController.getDormById);//pass
 router.get('/api/dorms/facility/:dorm_id', dormController.getFacilitiesOfDorm_api);//pass
-router.put('/api/dorms/facility/:user_id', upload.single("icon"), dormController.updateFacility_api);//pass
+router.put('/api/dorms/facility/:user_id', verifyToken, upload.single("icon"), dormController.updateFacility_api);//pass
 
 
 
