e9628b54309af6df53237bbe2aa31badcacb0b9e
PhattadonBS <phattadon232@gmail.com>
2026-08-13 14:50:46 +0700

    fix bugs

--- src/database/pool.ts
+++ src/database/pool.ts
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const dbcon = mysql.createPool({
-  host: process.env.DB_HOST || '', 
-  user: process.env.DB_USER || '',     
+  host: process.env.DB_HOST || 'localhost', 
+  user: process.env.DB_USER || 'root',     
   password: process.env.DB_PASSWORD || '',  
-  database: process.env.DB_NAME || '',
-  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME || 'huntpuk_db',
+  port: Number(process.env.DB_PORT) || 3306,
   waitForConnections: true,
   connectionLimit: 150,
   maxIdle: 150,
